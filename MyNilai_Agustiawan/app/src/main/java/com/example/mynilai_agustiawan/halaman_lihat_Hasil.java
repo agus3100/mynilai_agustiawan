eb551117c07b24500f970f3955b499f609f2ee67
agus3100 <109251111+agus3100@users.noreply.github.com>
2022-07-14 05:33:39 +0700

    Add files via upload

---
+++ MyNilai_Agustiawan/app/src/main/java/com/example/mynilai_agustiawan/halaman_lihat_Hasil.java
@@ -0,0 +1,15 @@
+package com.example.mynilai_agustiawan;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class halaman_lihat_Hasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_lihat_hasil);
+    }
+}
